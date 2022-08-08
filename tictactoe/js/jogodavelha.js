86405a2dd1898bc91ced5efc99fe2e0e0dd9ff25
Gabriel Pezzotti <hrb.7cs@gmail.com>
2022-08-08 08:33:18 -0300

    nao funciona mas ta bom

--- tictactoe/js/jogodavelha.js
+++ tictactoe/js/jogodavelha.js
@@ -1,27 +1,19 @@
 var letra = 'X';
 
 function joga(celula) {
-    celulaclicada= document.getElementById(celula).innerHTML;
+    celulaclicada = document.getElementById(celula).innerHTML;
     if (celulaclida == 'X' || celulaclicada == 'O') {
         alert("Este quadrado já foi escolhido");
     } else {
         document.getElementById(celula).innerHTML = letra;
         if (letra=='X'){
-            letra='O'
+            letra ='O'
         } else {
-            letra='X'
+            letra ='X'
         }
     }
 }
 
-function novo() {
-    for (i=1; i<4 ; i++) {
-        for(j=1; j<4; j++) {
-            nomecelula = 'cel' + i + j
-            document.getElementById(nomecelula).innerHTML = '';
-        }
-    }
-}
 function verif() {
     c11 = document.getElementById('cell11').innerHTML; //1 2 3  11 12 13
     c12 = document.getElementById('cell12').innerHTML; //4 5 6  21 22 23
@@ -32,15 +24,23 @@ function verif() {
     c31 = document.getElementById('cell31').innerHTML;
     c32 = document.getElementById('cell32').innerHTML;
     c33 = document.getElementById('cell33').innerHTML; 
-    if (((c11 != '') && (c12 != '') && (c13 != '') && (c11 == c12) && (c12 == c13) ) || // 1 2 3
-        ((c11 != '') && (c22 != '') && (c33 != '') && (c11 == c22) && (c22 == c33)) || // 1 5 9
-        ((c11 != '') && (c21 != '') && (c31 != '') && (c11 == c21) && (c21 == c31)) || // 1 4 7
-        ((c21 != '') && (c22 != '') && (c23 != '') && (c21 == c22) && (c22 == c23)) || // 4 5 6
-        ((c31 != '') && (c32 != '') && (c33 != '') && (c31 == c32) && (c32 == c33)) || // 7 8 9
-        ((c12 != '') && (c22 != '') && (c32 != '') && (c12 == c22) && (c22 == c32)) || // 2 5 8
-        ((c13 != '') && (c22 != '') && (c31 != '') && (c13 == c22) && (c22 == c31)) || // 3 5 7
-        ((c13 != '') && (c23 != '') && (c33 != '') && (c13 == c23) && (c23 == c33)) ) { // 3 6 9 
+    if( ((c11 != '') && (c12 != '') && (c13 != '') && (c11 == c12) && (c12 == c13)) || // 1 2 3
+    ((c11 != '') && (c22 != '') && (c33 != '') && (c11 == c22) && (c22 == c33)) || // 1 5 9
+    ((c11 != '') && (c21 != '') && (c31 != '') && (c11 == c21) && (c21 == c31)) || // 1 4 7
+    ((c21 != '') && (c22 != '') && (c23 != '') && (c21 == c22) && (c22 == c23)) || // 4 5 6
+    ((c31 != '') && (c32 != '') && (c33 != '') && (c31 == c32) && (c32 == c33)) || // 7 8 9
+    ((c12 != '') && (c22 != '') && (c32 != '') && (c12 == c22) && (c22 == c32)) || // 2 5 8
+    ((c13 != '') && (c22 != '') && (c31 != '') && (c13 == c22) && (c22 == c31)) || // 3 5 7
+    ((c13 != '') && (c23 != '') && (c33 != '') && (c13 == c23) && (c23 == c33)) ) { // 3 6 9 
         alert('You are the winner!');
         novo();
+    }
 }
+function novo() {
+    for (i=1; i<4 ; i++) {
+        for(j=1; j<4; j++) {
+            nomecelula = 'cell' + i + j;
+            document.getElementById(nomecelula).innerHTML = '';
+        }
+    }
 }
